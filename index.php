f0537b8d6b533daf9c4b040b81df47ddb873ab46
jodieumali <jodieumali15@gmail.com>
2019-06-08 12:14:36 +0800

    Added all files

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+        <meta http-equiv="X-UA-Compatible" content = "ie=edge">
+        <title>Jodie Umali</title>
+        <!-- CDN Files -->
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <!-- FontAwesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <!-- Local CSS -->
+        <link rel="stylesheet" type="text/css" href="styles/main.css"/>
+    </head>
+    
+    <body>
+        <div class="container-fluid main-container no-gutters">
+            
+            <div id="tohome" class="first-part-container">
+                <div class="row no-gutters">
+                    <nav id="navbar-container" class="d-flex justify-content-center align-items-center no-gutters">
+                        <img src="images/me_icon.png" width="140" height="78" alt="my_icon"/>
+                        <ul class="navigation d-flex align-items-center col-sm-8">
+                            <li><a href="#tohome">Home</a></li>
+                            <li><a href="#toabout">About</a></li>
+                            <li><a href="#toportfolio">Portfolio</a></li>
+                            <li><a href="#tocontact">Contact</a></li>
+                        </ul>
+                    </nav>
+                    
+                    <!--Mobile---------------------->
+                    <div class="navbar-mobile">
+                        <div class="head">
+                            <img src="images/me_icon_mobile.png" alt="my_icon" width="60"/>
+                            <!--<img class="hamburger" src="images/icons/menu.png" alt="hamburger_icon" width="60"/>-->
+                            <div class="icon">
+                                <div class="hamburger">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <nav class="nav-links">
+                            <ul>
+                                <li><a class="nav-click" href="#tohome">Home</a></li>
+                                <li><a class="nav-click" href="#toabout1">About</a></li>
+                                <li><a class="nav-click" href="#toportfolio1">Portfolio</a></li>
+                                <li><a class="nav-click" href="#tocontact">Contact</a></li>
+                            </ul>
+                            <div class="infos">
+                                <label class="other-info">Other Information</label>
+                                <div class="br-line"></div>
+                                <div class="info-child">
+                                    <img src="images/icons/mobile-call.png" alt="call_icon"/>
+                                    <label>09178710594</label>
+                                </div>
+                                <div class="info-child">
+                                    <img src="images/icons/mobile-mail.png" alt="mail_icon"/>
+                                    <label>jodieumali15@gmail.com</label>
+                                </div>
+                                <div class="info-child">
+                                    <img src="images/icons/mobile-location.png" alt="location_icon"/>
+                                    <label>Taytay Rizal</label>
+                                </div>
+                                <div class="info-child">
+                                    <img src="images/icons/mobile-date.png" alt="date_icon"/>
+                                    <label>November 12, 1997</label>
+                                </div>
+                            </div>                            
+                        </nav>
+                    </div>
+                    <!------------------------------>
+                    
+                </div> <!-- End of navigation -->
+
+                <div class="row no-gutters">
+                    <div class="home-content d-flex col-sm-10 offset-sm-1">
+                        <div class="left-content col-sm-6">
+                            <div class="section-holder">
+                                <section class="section1">
+                                    <label>Hello,</label>
+                                    <h1>I'm Jodie</h1>
+                                    <label>GRADUATE OF SAN BEDA UNIVERSITY</label>
+                                </section>
+                                <section class="section2">
+                                    <div class="d-flex align-items-center">
+                                        <img src="images/icons/call.png" width="30" alt="call_icon"/>
+                                        <label>09178710594</label>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <img src="images/icons/mail.png" width="30" alt="mail_icon"/>
+                                        <label>jodieumali15@gmail.com</label>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <img src="images/icons/location.png" width="30" alt="location_icon"/>
+                                        <label>Taytay Rizal</label>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <img src="images/icons/date.png" width="30" alt="date_icon"/>
+                                        <label>November 12, 1997</label>
+                                    </div>
+                                </section>
+                                <section class="resume">
+                                    <button><a href="#">Download Resume</a></button>
+                                </section>
+                            </div>                            
+                        </div> <!-- End of left container -->
+                        <div class="right-content col-sm-6">
+                            <img class="me1" src="images/img1.png" width="700" alt="picture_of_me"/>
+                        </div> <!-- End of right container -->
+                    </div>
+                </div> <!-- End of home content -->   
+                
+                <!--Mobile---------------------->
+                <div class="home-mobile">
+                    <img src="images/img1.png" alt="mypic" width="400px"/>
+                    <section class="section1">
+                        <label>Hello,</label>
+                        <h1>I'm Jodie</h1>
+                        <label>GRADUATE OF SAN BEDA UNIVERSITY</label>
+                        <button class="resume"><a href="#">Download Resume</a></button>
+                    </section>
+                </div>
+                <!------------------------------>
+                
+            </div> <!-- End of first part container -->
+            
+            <div id="toabout" class="second-part-container">
+                <div class="row no-gutters">
+                    <div class="about-content col-sm-12 d-flex">
+                        <div class="left-content1 col-sm-5">
+                            <img class="me2" src="images/img2.png" width="700" alt="picture_of_me"/>
+                        </div>
+                        <div class="right-content1 col-sm-7">
+                            <div class="about-holder">
+                                <section>
+                                    <h1>Somethin' About me</h1>
+                                </section>
+                                <section>
+                                    <p>A Self-motivated, Hardworking, Creative and a Multi-tasking person with enough knowledge and background in the field of UI/UX Development and front-end development especially HTML5, CSS3 and a some of Javascript. An enthusiastic person when it comes to learning something new. Enjoys creating and designing wonderful contents for websites and applications. Seeing unique aesthetic designs is what encourages him to create something better. </p>
+                                </section>
+                                <section class="section3 d-flex">
+                                    <a href="https://www.facebook.com/jodie.umali">
+                                        <img src="images/icons/fb.png" width="77" alt="fb_icon"/>
+                                    </a>
+                                    <a href="https://www.instagram.com/jodieumali/">
+                                        <img src="images/icons/instagram.png" width="77" alt="instagram_icon"/>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/ianne-jodie-umali-7a2652165/">
+                                        <img src="images/icons/linkedin.png" width="77" alt="linkedin_icon"/>
+                                    </a>
+                                </section>
+                            </div>                            
+                        </div>
+                    </div>                                               
+                </div> <!-- End of about content -->   
+                
+                <img class="stud" src="images/STUDIOUS.png" alt="studious_pic"/>   
+            </div> <!-- End of second part container -->
+            
+            <div id="toabout1"></div>
+            
+            <!--Mobile---------------------->
+                <div class="about-mobile">
+                    <div class="about-holder">
+                        <section>
+                            <h1>Somethin' About me</h1>
+                        </section>
+                        <section>
+                            <p>A Self-motivated, Hardworking, Creative and a Multi-tasking person with enough knowledge and background in the field of UI/UX Development and front-end development especially HTML5, CSS3 and a some of Javascript. An enthusiastic person when it comes to learning something new. Enjoys creating and designing wonderful contents for websites and applications. Seeing unique aesthetic designs is what encourages him to create something better. </p>
+                        </section>
+                        <section class="section3 d-flex justify-content-center">
+                            <a href="https://www.facebook.com/jodie.umali">
+                                <img src="images/icons/fb.png" width="77" alt="fb_icon"/>
+                            </a>
+                            <a href="https://www.instagram.com/jodieumali/">
+                                <img src="images/icons/instagram.png" width="77" alt="instagram_icon"/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/ianne-jodie-umali-7a2652165/">
+                                <img src="images/icons/linkedin.png" width="77" alt="linkedin_icon"/>
+                            </a>
+                        </section>
+                    </div>                    
+                </div>
+                <!------------------------------>
+            
+            <div id="toportfolio1"></div>
+            
+            <div id="toportfolio" class="third-part-container">
+                <div class="row no-gutters">
+                    <div class="portfolio-content col-sm-10 offset-sm-1">
+                        <div class="portfolio-title d-flex justify-content-center">
+                            <h1>Portfolio</h1>
+                        </div>
+                        <div class="portfolio-works">
+                            <div class="box">
+                                <div class="to-works">
+                                    <label>Mobile and Web Designs</label>
+                                    <a href="#"><img class="arrow" src="images/arrow.png" alt="arrow"/></a>
+                                </div>
+                            </div>
+                            <div class="box">
+                                <div class="to-works">
+                                    <label>Photography Collections</label>
+                                    <a href="#"><img class="arrow" src="images/arrow.png" alt="arrow"/></a>
+                                </div>
+                            </div>
+                            <div class="box">
+                                <div class="to-works">
+                                    <label>Graphic Designs</label>
+                                    <a href="#"><img class="arrow" src="images/arrow.png" alt="arrow"/></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>                
+            </div> <!-- End of third part container -->
+            
+            <div id="tocontact" class="fourth-part-container">
+                <div class="row no-gutters">
+                    <div class="footer-content col-sm-10 offset-sm-1">
+                        <div class="to-email">
+                            <label>Hire me! Say Hello at</label>
+                            <h1>jodieumali15@gmail.com</h1>
+                        </div>
+                        <div class="icons">
+                            <a href="https://www.facebook.com/jodie.umali"><img src="images/icons/fb-foot.png" width="24" alt="fb_icon"/></a>
+                            <a href="https://twitter.com/umalijodie"><img src="images/icons/twitter-foot.png" width="24" alt="twitter_icon"/></a>
+                            <a href="https://www.instagram.com/jodieumali/"><img src="images/icons/instagram-foot.png" width="24" alt="instagram_icon"/></a>
+                            <a href="https://www.linkedin.com/in/ianne-jodie-umali-7a2652165/"><img src="images/icons/linkedin-foot.png" width="24" alt="linkedin_icon"/></a>
+                        </div>
+                        <div class="copyright">
+                            <h4>Ianne Jodie Umali 2019, All rights reserved.</h4>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- End of fourth part container -->
+            
+        </div> <!-- End of main container -->
+        
+        <!-- CDN Files -->
+        <!-- Jquery -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <!-- Local Files -->
+        <script src="scripts/script.js"></script>
+    </body>
+</html>
+
---
+++ scripts/script.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+    
+    navbarscroll();
+    menu();
+   
+    function navbarscroll(){
+        $(window).on("scroll",function(){
+        
+            var vscroll = $(this).scrollTop();
+
+            if(vscroll > 125){
+                $("#navbar-container").addClass('navbg');
+            }
+            else{
+                $("#navbar-container").removeClass('navbg');
+            }
+
+        });
+    }
+    
+    function menu(){
+        $(".icon").on("click",function(){
+           $(".navbar-mobile").toggleClass("navbar-mobile-on");
+            $(".nav-links").toggleClass('show-links');
+            $(this).toggleClass('active');
+            $(".hamburger").toggleClass('hamburger-on-click');
+            $(".hamburger").toggleClass('active');
+        });
+        
+        $(".nav-click").on("click",function(){
+            $(".navbar-mobile").removeClass("navbar-mobile-on");
+            $(".nav-links").removeClass('show-links'); 
+        });
+    }
+
+});
+
+/*$(document).ready(function(){
+    
+    parallaxscroll();
+    remove();
+    
+    function parallaxscroll(){
+        
+        $(window).scroll(function(){
+            
+            var vscroll = $(this).scrollTop();
+            
+            $(".title").css({
+                'transform' : 'translate(0px, '+ vscroll/10 +'%)'
+            });
+            
+            $(".lantern1").css({
+                'transform' : 'translate(0px, '+ vscroll/5 +'%)'
+            });
+            
+            $(".lantern2").css({
+                'transform' : 'translate(0px, -'+ vscroll/10 +'%)'
+            });
+            
+            $(".lantern3").css({
+                'transform' : 'translate(0px, '+ vscroll/5 +'%)'
+            });
+            
+            $(".lantern4").css({
+                'transform' : 'translate(0px, -'+ vscroll/10 +'%)'
+            });
+            
+            if(vscroll > $('.menubox').offset().top - ($(window).height() / 1.2)){ 
+                
+                $('.kiki').each(function(i){
+                    
+                    setTimeout(function(){
+                        $('.kiki').eq(i).addClass('show'); 
+                    }, 150 * (i+1));
+                    
+                });
+            }
+            
+        });
+        
+    }
+    
+    function remove(){
+        
+        $(window).scroll(function(){
+            
+            var wscroll = $(this).scrollTop();
+            
+            if(wscroll > 650){
+                $(".lantern1").css('display', 'none');
+            }
+            else{
+                $(".lantern1").css('display', 'block');
+            }
+            
+        });
+        
+    }
+    
+});*/
